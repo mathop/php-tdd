8346e48ce2f81155671bb7ad5f371eb2aaa4f09d
Matheus Pedroso <matheus@mathop.com>
2013-06-17 23:13:54 -0300

    Mais testes para classe MaiorEMenor.

--- src/Mathop/Chapter2/TestaMaiorEMenor.php
+++ src/Mathop/Chapter2/TestaMaiorEMenor.php
@@ -21,6 +21,32 @@
             $this->assertEquals('Jogo de pratos', $algoritmo->getMenor()->getNome());
             $this->assertEquals('Geladeira', $algoritmo->getMaior()->getNome());
         }
+
+        public function testOrdemCrescente()
+        {
+            $carrinho = new CarrinhoDeCompras();
+            $carrinho->adiciona(new Produto('Jogo de pratos', 70.00));
+            $carrinho->adiciona(new Produto('Liquidificador', 250.00));
+            $carrinho->adiciona(new Produto('Geladeira', 450.00));
+
+            $algoritmo = new MaiorEMenor();
+            $algoritmo->encontra($carrinho);
+
+            $this->assertEquals('Jogo de pratos', $algoritmo->getMenor()->getNome());
+            $this->assertEquals('Geladeira', $algoritmo->getMaior()->getNome());
+        }
+
+        public function testApenasUmProduto()
+        {
+            $carrinho = new CarrinhoDeCompras();
+            $carrinho->adiciona(new Produto('Geladeira', 450.00));
+
+            $algoritmo = new MaiorEMenor();
+            $algoritmo->encontra($carrinho);
+
+            $this->assertEquals('Geladeira', $algoritmo->getMenor()->getNome());
+            $this->assertEquals('Geladeira', $algoritmo->getMaior()->getNome());
+        }
     }
 
     new TestaMaiorEMenor();
