b8438b01a6e4b5843063fd97f0bb597002df9097
Matheus Pedroso <matheus@mathop.com>
2013-06-24 23:03:20 -0300

    Implementação no método calculaSalario para calcular salário de DBA.

--- src/Mathop/Chapter7/CalculadoraDeSalario.php
+++ src/Mathop/Chapter7/CalculadoraDeSalario.php
@@ -6,9 +6,17 @@
     {
         public function calculaSalario(Funcionario $funcionario)
         {
-            if ($funcionario->getSalario() > 3000) {
-                return $funcionario->getSalario() * 0.8;
+            if ($funcionario->getCargo() == Cargo::DESENVOLVEDOR) {
+                if ($funcionario->getSalario() > 3000) {
+                    return $funcionario->getSalario() * 0.8;
+                }
+                return $funcionario->getSalario() * 0.9;
             }
-            return $funcionario->getSalario() * 0.9;
+            else if ($funcionario->getCargo() == Cargo::DBA) {
+                if ($funcionario->getSalario() < 2500) {
+                    return $funcionario->getSalario() * 0.85;
+                }
+                return $funcionario->getSalario() * 0.75;
+            }
         }
     }
