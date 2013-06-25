eb1fce0b43d846849545de005b38659d0847d0ad
Matheus Pedroso <matheus@mathop.com>
2013-06-24 23:11:37 -0300

    Refatoração do método CalculadoraDeSalario::calculaSalario().

--- src/Mathop/Chapter7/CalculadoraDeSalario.php
+++ src/Mathop/Chapter7/CalculadoraDeSalario.php
@@ -7,18 +7,28 @@
         public function calculaSalario(Funcionario $funcionario)
         {
             if ($funcionario->getCargo() == Cargo::DESENVOLVEDOR) {
-                if ($funcionario->getSalario() > 3000) {
-                    return $funcionario->getSalario() * 0.8;
-                }
-                return $funcionario->getSalario() * 0.9;
+                return $this->dezOuVintePorCentoDeDesconto($funcionario);
             }
             else if ($funcionario->getCargo() == Cargo::DBA || $funcionario->getCargo() == Cargo::TESTADOR) {
-                if ($funcionario->getSalario() < 2500) {
-                    return $funcionario->getSalario() * 0.85;
-                }
-                return $funcionario->getSalario() * 0.75;
+                return $this->quinzeOuVinteCincoPorCentoDeDesconto($funcionario);
             }
 
             throw new Exception('Funcionário inválido');
+        }
+
+        private function quinzeOuVinteCincoPorCentoDeDesconto(Funcionario $funcionario)
+        {
+            if ($funcionario->getSalario() < 2500) {
+                return $funcionario->getSalario() * 0.85;
+            }
+            return $funcionario->getSalario() * 0.75;
+        }
+
+        private function dezOuVintePorCentoDeDesconto(Funcionario $funcionario)
+        {
+            if ($funcionario->getSalario() > 3000) {
+                return $funcionario->getSalario() * 0.8;
+            }
+            return $funcionario->getSalario() * 0.9;
         }
     }
