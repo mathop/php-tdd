bed9a6cef546920645516201cc18f0053c7c8f30
Matheus Pedroso <matheus@mathop.com>
2013-06-29 17:18:06 -0300

    Adicionado teste para persistência da nota fiscal.

--- src/Mathop/Chapter8/GeradorDeNotaFiscal.php
+++ src/Mathop/Chapter8/GeradorDeNotaFiscal.php
@@ -4,12 +4,18 @@
 
     class GeradorDeNotaFiscal
     {
+        private $dao;
+
+        public function __construct(\NFDao $dao)
+        {
+            $this->dao = $dao;
+        }
+
         public function gera(Pedido $pedido)
         {
             $nf = new NotaFiscal($pedido->getCliente(), $pedido->getValorTotal() * 0.94, new \DateTime());
-            $nfdao = new NFDao();
 
-            $nfdao->persiste($nf);
+            $this->dao->persiste($nf);
 
             return $nf;
         }
--- src/Mathop/Chapter8/GeradorDeNotaFiscalTest.php
+++ src/Mathop/Chapter8/GeradorDeNotaFiscalTest.php
@@ -1,6 +1,7 @@
 <?php
 
     use Mathop\Chapter8\GeradorDeNotaFiscal;
+    use Mathop\Chapter8\NFDao;
     use Mathop\Chapter8\Pedido;
 
     require_once('../../../vendor/autoload.php');
@@ -9,11 +10,26 @@
     {
         public function testDeveGerarNFComValorDeImpostoDescontado()
         {
-            $gerador = new GeradorDeNotaFiscal();
+            $dao = $this->getMock('NFDao', array('persiste'));
+
+            $gerador = new GeradorDeNotaFiscal($dao);
             $pedido = new Pedido('Matheus', 1000, 1);
 
             $nf = $gerador->gera($pedido);
 
             $this->assertEquals(1000 * 0.94, $nf->getValor(), 0.00001);
+        }
+
+        public function testDevePersistirNFGerada()
+        {
+            $dao = $this->getMock('NFDao', array('persiste'));
+
+            $dao->expects($this->once())
+                ->method('persiste');
+
+            $gerador = new GeradorDeNotaFiscal($dao);
+            $pedido = new Pedido('Matheus', 1000, 1);
+
+            $nf = $gerador->gera($pedido);
         }
     }
