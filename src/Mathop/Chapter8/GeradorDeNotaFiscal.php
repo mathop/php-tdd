f4401fe73c3d7aefd5aa781364706ecb7eea3913
Matheus Pedroso <matheus@mathop.com>
2013-06-29 15:32:30 -0300

    Adicionada persistência na geração da nota fiscal.

--- src/Mathop/Chapter8/GeradorDeNotaFiscal.php
+++ src/Mathop/Chapter8/GeradorDeNotaFiscal.php
@@ -6,6 +6,11 @@
     {
         public function gera(Pedido $pedido)
         {
-            return new NotaFiscal($pedido->getCliente(), $pedido->getValorTotal() * 0.94, new \DateTime());
+            $nf = new NotaFiscal($pedido->getCliente(), $pedido->getValorTotal() * 0.94, new \DateTime());
+            $nfdao = new NFDao();
+
+            $nfdao->persiste($nf);
+
+            return $nf;
         }
     }
--- src/Mathop/Chapter8/NFDao.php
+++ src/Mathop/Chapter8/NFDao.php
@@ -1,5 +1,7 @@
 <?php
 
+    namespace Mathop\Chapter8;
+
     class NFDao
     {
         public function persiste(NotaFiscal $nf)
